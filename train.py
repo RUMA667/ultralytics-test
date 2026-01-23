5fbb24c52dfd99318b128aa9a4dd43bf12465e9f
RUMA667 <RUMAcould@21mail.qzz.io>
2026-01-23 02:19:52 -0500

    Create train.py
    Signed-off-by: RUMA667 <RUMAcould@21mail.qzz.io>

---
+++ train.py
@@ -0,0 +1,31 @@
+import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO('/kaggle/working/ultralytics/ultralytics/cfg/models/11/yolo11s-oper03.yaml')
+    #model.load('/content/drive/MyDrive/KaggleNotebookOutput/ultralytics/runs/train/exp/weights/best.pt') # loading pretrain weights
+    #model.load('/kaggle/input/tumor-yolo/finally-best.pt') # loading pretrain weights
+    model.train(#data='/root/.cache/kagglehub/datasets/monaerkiconbinker/tumor-yolo/versions/2/breast_tumor04/data.yaml',
+                #data='/kaggle/input/US-needle/augmented/data.yaml',
+                data='/kaggle/input/YOLO-tumor/my_yolo_dataset/data.yaml',
+                cache=False,
+                imgsz=640,
+                epochs=450,
+                batch= 32,
+                close_mosaic=450,
+                device='0,1',
+                project='runs/train',
+                name='exp',
+                patience=75,
+                workers=0,
+                val=True,
+                amp=False,
+                box=15.0,
+                warmup_epochs=5,
+                optimizer='SGD', 
+                #save_period = 20,
+                
+                #python train.py --img 640 --batch 16 --epochs 400 --data your_dataset.yaml --weights yolov8n.pt --patience 100 --close_mosaic 0 --name Stage1_Mosaic_Training
+                #python train.py --img 640 --batch 16 --epochs 50 --data your_dataset.yaml --weights runs/train/Stage1_Mosaic_Training/weights/best.pt --patience 30 --lr0 0.001 --no-aug --name Stage2_Finetuning
+                )
